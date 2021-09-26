1eaddfd95560d35f52d2a2eca439c5334a23a348
yyccQQu <Email.qq.com>
2021-09-26 12:07:41 +0400

    no message

--- 9mnist/script.js
+++ 9mnist/script.js
@@ -2,16 +2,19 @@ import * as tf from '@tensorflow/tfjs';
 import * as tfvis from '@tensorflow/tfjs-vis';
 import { MnistData } from './data';
 
+// hs data --cors
+
 window.onload = async () => {
     const data = new MnistData();
     await data.load();
     const examples = data.nextTestBatch(20);
     const surface = tfvis.visor().surface({ name: '输入示例' });
+    // 20张图片
     for (let i = 0; i < 20; i += 1) {
-        const imageTensor = tf.tidy(() => {
+        const imageTensor = tf.tidy(() => { // 清除webGL中 没用的tensor内存
             return examples.xs
-                .slice([i, 0], [1, 784])
-                .reshape([28, 28, 1]);
+                .slice([i, 0], [1, 784]) // 第i个图片的第1个像素值，只切割一个图片，切割刀784个像素
+                .reshape([28, 28, 1]); // 重新更改图片形状 宽28 高28 黑白图片1
         });
 
         const canvas = document.createElement('canvas');
@@ -19,22 +22,26 @@ window.onload = async () => {
         canvas.height = 28;
         canvas.style = 'margin: 4px';
         await tf.browser.toPixels(imageTensor, canvas);
+        //document.body.appendChild(canvas)
         surface.drawArea.appendChild(canvas);
     }
 
     const model = tf.sequential();
+    // 卷积层
     model.add(tf.layers.conv2d({
         inputShape: [28, 28, 1],
-        kernelSize: 5,
-        filters: 8,
-        strides: 1,
-        activation: 'relu',
-        kernelInitializer: 'varianceScaling'
+        kernelSize: 5, // 卷积核大小
+        filters: 8, // 超参数
+        strides: 1, // 移动步长
+        activation: 'relu', // 如果小于0则舍弃，反之使用
+        kernelInitializer: 'varianceScaling' // 卷积核初始化
     }));
+    // 最大池化 层
     model.add(tf.layers.maxPool2d({
-        poolSize: [2, 2],
+        poolSize: [2, 2], // 池化尺寸 2x2
         strides: [2, 2]
     }));
+    // 卷积层 再次提取特征
     model.add(tf.layers.conv2d({
         kernelSize: 5,
         filters: 16,
@@ -46,17 +53,18 @@ window.onload = async () => {
         poolSize: [2, 2],
         strides: [2, 2]
     }));
-    model.add(tf.layers.flatten());
+    model.add(tf.layers.flatten()); // 把高维数据 摊平 放到一维数组里
+    // 最后全链接层 分类
     model.add(tf.layers.dense({
         units: 10,
         activation: 'softmax',
         kernelInitializer: 'varianceScaling'
     }));
     model.compile({
-        loss: 'categoricalCrossentropy',
-        optimizer: tf.train.adam(),
-        metrics: ['accuracy']
-    });
+		loss: "categoricalCrossentropy", // 交叉熵损失函数处理多分类问题
+		optimizer: tf.train.adam(),
+		metrics: ["accuracy"], // 准确度度量
+	});
 
     const [trainXs, trainYs] = tf.tidy(() => {
         const d = data.nextTrainBatch(1000);
@@ -74,6 +82,7 @@ window.onload = async () => {
         ];
     });
 
+    //进行训练
     await model.fit(trainXs, trainYs, {
         validationData: [testXs, testYs],
         batchSize: 500,
@@ -118,3 +127,15 @@ window.onload = async () => {
         console.log(`预测结果为 ${pred.dataSync()[0]}`);
     };
 };
+
+
+// 池化层 用于提取最强的特征
+// 扩大感受视野，减少计算量
+// 池化层没有权重需要训练
+
+// 全链接层
+// 作为输出层 作为分类器 全链接层权重需要训练
+
+
+
+
